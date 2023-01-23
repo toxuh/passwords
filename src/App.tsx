23fc2a612fa680a4bc0ffe47ea899a7cb14e6297
Andrey Kashin <askashin@xaru.io>
2023-01-23 16:15:29 +0700

    checkbox + const

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import "./App.sass";
 
 function App() {
   let letters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()";
+
   const [randomPassword, setRandomPassword] = useState("");
   const [passwordLength, setPasswordLength] = useState(10);
   const [isAddCapitalLetters, setIsAddCapitalLetters] = useState(false);
@@ -18,10 +21,10 @@ function App() {
       letters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
     if (isNumbers) {
-      letters += "0123456789";
+      letters += numbers;
     }
     if (isSymbol) {
-      letters += "!@#$%^&*()";
+      letters += symbols;
     }
 
     for (let i = 0; i < passwordLength; i++) {
@@ -33,9 +36,6 @@ function App() {
 
   const passwordArray = randomPassword.split("");
 
-  const numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const symbolArray = [",!,@,#,$,%,^,&,*,(,),"];
-
   return (
     <div className="GeneratePassword">
       <div className="GeneratePasswordWrapper">
@@ -46,8 +46,8 @@ function App() {
             ? passwordArray.map((item, i) => (
                 <li
                   className={`PasswordList__item ${
-                    numbersArray.includes(Number(item)) ? "NumberItem" : ""
-                  } ${symbolArray.includes(item) ? "SymbolItem" : ""}`}
+                    numbers.split("").includes(item) ? "NumberItem" : ""
+                  } ${symbols.split("").includes(item) ? "SymbolItem" : ""}`}
                   key={i}
                 >
                   {item}
@@ -70,34 +70,43 @@ function App() {
         <h3>Settings</h3>
         <div className="SettingsWrapper">
           <div>Include Uppercase</div>
-          <input
-            className="CheckBox"
-            type="checkbox"
-            checked={isAddCapitalLetters}
-            onChange={(e) => setIsAddCapitalLetters(e.target.checked)}
-          />
+          <label className="Switch">
+            <input
+              className="Checkbox"
+              type="checkbox"
+              checked={isAddCapitalLetters}
+              onChange={(e) => setIsAddCapitalLetters(e.target.checked)}
+            />
+            <span className="Slider"></span>
+          </label>
         </div>
         <div className="SettingsWrapper">
           <div>Include Numbers</div>
-          <input
-            className="CheckBox"
-            type="checkbox"
-            checked={isNumbers}
-            onChange={(e) => {
-              setIsNumbers(e.target.checked);
-            }}
-          />
+          <label className="Switch">
+            <input
+              className="Checkbox"
+              type="checkbox"
+              checked={isNumbers}
+              onChange={(e) => {
+                setIsNumbers(e.target.checked);
+              }}
+            />
+            <span className="Slider"></span>
+          </label>
         </div>
         <div className="SettingsWrapper">
           <div>Include Symbols</div>
-          <input
-            className="CheckBox"
-            type="checkbox"
-            checked={isSymbol}
-            onChange={(e) => {
-              setIsSymbol(e.target.checked);
-            }}
-          />
+          <label className="Switch">
+            <input
+              className="Checkbox"
+              type="checkbox"
+              checked={isSymbol}
+              onChange={(e) => {
+                setIsSymbol(e.target.checked);
+              }}
+            />
+            <span className="Slider"></span>
+          </label>
         </div>
         <button className="GenerateButton" onClick={generate}>
           Generate password
